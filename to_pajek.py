85a14779afc932ff3dc8c74e50cfc3ab2fe411cb
kronii <andrej.kronovsek@gmail.com>
2023-01-10 09:29:42 +0100

    Add support for converting from other locations

--- to_pajek.py
+++ to_pajek.py
@@ -1,45 +1,56 @@
 from collections import defaultdict, OrderedDict
+import argparse
 
-filename = "example.txt"
+def main(args):
+    filename = "example.txt"
 
-PATH_IN = "./data/original/"
-PATH_OUT = "./data/pajek/"
+    PATH_IN = args.path_in
+    PATH_OUT = args.path_out
 
-with open(PATH_IN + filename) as f:
-    lines = f.readlines()
+    with open(PATH_IN + filename) as f:
+        lines = f.readlines()
 
-nodes = set()
-connections = defaultdict(list)
-for line in lines:
-    s = line.split()
+    nodes = set()
+    connections = defaultdict(list)
+    for line in lines:
+        s = line.split()
 
-    from_node = int(s[2])
-    to_node = int(s[0])
+        from_node = int(s[2])
+        to_node = int(s[0])
 
-    # Add both nodes to set of nodes
-    nodes.add(from_node)
-    nodes.add(to_node)
+        # Add both nodes to set of nodes
+        nodes.add(from_node)
+        nodes.add(to_node)
 
-    # Append to connections
-    connections[from_node].append(to_node)
+        # Append to connections
+        connections[from_node].append(to_node)
 
-# Order connections by node id inc
-connections = dict(OrderedDict(sorted(connections.items())))
+    # Order connections by node id inc
+    connections = dict(OrderedDict(sorted(connections.items())))
 
-# Write to file
-with open(PATH_OUT + filename, 'w') as f:
+    # Write to file
+    with open(PATH_OUT + filename, 'w+') as f:
 
-    f.write("*Vertices " + str(len(nodes)) + "\n")
+        f.write("*Vertices " + str(len(nodes)) + "\n")
 
-    nodes_ordered = sorted(list(nodes))
+        nodes_ordered = sorted(list(nodes))
 
-    for node in nodes_ordered:
-        f.write(str(node) + " " + str(node))
-        f.write('\n')
-
-    f.write("*arcs " + "\n")
-
-    for from_node in connections:
-        for to_node in connections[from_node]:
-            f.write(str(from_node) + " " + str(to_node))
+        for node in nodes_ordered:
+            f.write(str(node) + " " + str(node))
             f.write('\n')
+
+        f.write("*arcs " + "\n")
+
+        for from_node in connections:
+            for to_node in connections[from_node]:
+                f.write(str(from_node) + " " + str(to_node))
+                f.write('\n')
+
+
+if __name__=='__main__':
+
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--path_in', type=str, default='./data/original/')
+    parser.add_argument('--path_out', type=str, default='./data/pajek/')
+    args=parser.parse_args()
+    main(args)
